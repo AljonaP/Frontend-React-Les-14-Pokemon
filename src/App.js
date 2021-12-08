2b2832bbd5d0aba236332164931e708920712690
AljonaP <Bartosh_78@hotmail.com>
2021-12-08 15:32:37 +0100

    pokemon jigglypuff

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import axios from "axios";
 import './App.css';
+import React, {useState, useEffect} from "react";
+import Pokemon from "./components/Pokemon";
+
 
 function App() {
+
+    const [pokemonData, setPokemonData] = useState('');
+    // const [endPoint, setEndPoint] = useState(`https://pokeapi.co/api/v2/pokemon/jigglypuff`);
+    // const [endPoint, setEndPoint] = useState(`https://pokeapi.co/api/v2/pokemon/?offset=20&limit=20`);
+
+
+    useEffect(() => {
+
+
+    async function fetchData() {
+        try {
+            const result = await axios.get(endPoint)
+            console.log(result.data);
+            setPokemonData(result.data);
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    fetchData()
+    console.log('start!');
+}, [])
+
   return (
-    <div>
-      Begin hier met de opdracht!
-    </div>
+      <Pokemon endpoint={endPoint}     />
+
   );
 }
 
---
+++ src/components/Pokemon.js
@@ -0,0 +1,41 @@
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
+
+
+    const Pokemon = ({endpoint}) => {
+    const [pokemonData, setPokemonData] = useState('');
+
+    useEffect(() => {
+
+
+        async function fetchData() {
+            try {
+                const result = await axios.get(endpoint)
+                console.log(result.data);
+                setPokemonData(result.data);
+            } catch (e) {
+                console.error(e);
+            }
+        }
+
+        fetchData()
+        console.log('start!');
+    }, [])
+
+    return (
+        <div>
+            {pokemonData && <>
+                <h3>{pokemonData.name}</h3>
+                <img src={pokemonData.sprites.front_default} alt="jigglypuff-image"/>
+
+                {pokemonData.abilities.map((pokemon) => {
+                    return (
+                        <li>{pokemon.ability.name}</li>)
+                })}
+            </>}
+        </div>
+    )
+}
+
+
+export default Pokemon;
