a300c6fcd467643fcaf2e2b0bd6b05a3ed6b901d
AljonaP <Bartosh_78@hotmail.com>
2021-12-14 11:19:42 +0100

    everything has been styled in CSS :-D

--- src/components/Pokemon/Pokemon.js
+++ src/components/Pokemon/Pokemon.js
@@ -28,14 +28,14 @@ import axios from "axios";
                 <h3>{pokemonData.name}</h3>
                 <img src={pokemonData.sprites.front_default} alt="jigglypuff-image"/>
                 <div className="weight-moves">
-                    <h4 className="moves">Moves: <span className="light">{pokemonData.moves.length}</span></h4>
+                    <h4>Moves: <span className="light">{pokemonData.moves.length}</span></h4>
                     <h4>Weight: <span className="light">{pokemonData.weight}</span></h4>
                 </div>
 
                 <h4>Abilities:</h4>
                 {pokemonData.abilities.map((pokemon) => {
                     return (
-                        <li className="ability">{pokemon.ability.name}</li>)
+                        <li>{pokemon.ability.name}</li>)
                 })}
             </>}
         </div>
